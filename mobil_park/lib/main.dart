8391e0e7e3f8c9cb988fed73cfbfb2d520aede97
Rishit Rathod <rishitrathod52@gmail.com>
2024-11-20 14:39:06 +0530

    main update

--- mobil_park/lib/main.dart
+++ mobil_park/lib/main.dart
@@ -1,6 +1,6 @@
- import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mobil_park/services/firebase_options.dart';
+import 'package:mobil_park/firebase_options.dart';
 
 Future<void> main() async {
 
